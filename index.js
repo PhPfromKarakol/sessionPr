bedafc0cc4665ca48b0006c6da0461ff8ce9a76c
Nurbek Abdykerimov <104121143+PhPfromKarakol@users.noreply.github.com>
2023-09-20 03:14:54 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const sessionOptions = {
+    secret: "thisisnotagoodsecret",
+    resave: false,
+    saveUninitialized: false,
+}
+
+app.use(session(sessionOptions));
+
+app.get('/viewcount', (req, res) => {
+    if (req.session.count) {
+        req.session.count += 1;
+    }
+    else {
+        req.session.count = 1;
+    }
+
+    res.send(`you have viewed this page ${req.session.count} times`);
+})
+
+app.get('/register', (req, res) => {
+    const { username = 'mal' } = req.query;
+    req.session.username = username;
+    res.redirect('/greet');
+
+});
+
+app.get('/greet', (req, res) => {
+    const { username } = req.session
+    res.send(`Aleykum salam ${username}`);
+});
+
+
+
+app.listen(3000, () => {
+    console.log('listeting port 3000');
+})
